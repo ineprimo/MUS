8e4eddc3fcd78b8dedee8a82625a7ad83e8734e5
Adelpozo04 <addelp01@ucm.es>
2025-05-18 03:33:47 +0200

    acabado proyecto de unity

--- PanelSonidosUnity/PanelSonidoD&D/Assets/Scripts/ControladorMusica.cs
+++ PanelSonidosUnity/PanelSonidoD&D/Assets/Scripts/ControladorMusica.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ControladorMusica : MonoBehaviour
 {
 
+    [SerializeField] private TextMeshProUGUI[] _textosBiomas;
+
+    [SerializeField] private TextMeshProUGUI[] _textosSentimientos;
+
     [SerializeField] private Slider _intensidadSlider;
+
+    [SerializeField] private Slider _volumenSlider;
+
 
     FMODUnity.StudioEventEmitter _musica;
 
@@ -19,12 +27,26 @@ public class ControladorMusica : MonoBehaviour
         _musica.EventInstance.getParameterByName("Intensidad", out intensidadInicio);
 
         _intensidadSlider.value = intensidadInicio;
+
+        float volumenInicio;
+        _musica.EventInstance.getVolume(out volumenInicio);
+
+        _volumenSlider.value = volumenInicio;
     }
 
     public void cambiarBioma(int bioma)
     {
 
         _musica.EventInstance.setParameterByName("Bioma", bioma);
+
+        for(int i = 0; i < _textosBiomas.Length; i++)
+        {
+
+            _textosBiomas[i].color = Color.white;
+
+        }
+
+        _textosBiomas[bioma].color = Color.yellow;
 
     }
 
@@ -33,12 +55,28 @@ public class ControladorMusica : MonoBehaviour
 
         _musica.EventInstance.setParameterByName("Sentimiento", sentimiento);
 
+        for (int i = 0; i < _textosSentimientos.Length; i++)
+        {
+
+            _textosSentimientos[i].color = Color.white;
+
+        }
+
+        _textosSentimientos[sentimiento].color = Color.yellow;
+
     }
 
     public void cambiarIntensidad()
     {
 
         _musica.EventInstance.setParameterByName("Intensidad", _intensidadSlider.value);
+
+    }
+
+    public void cambiarVolumen()
+    {
+
+        _musica.EventInstance.setVolume(_volumenSlider.value);
 
     }
 
